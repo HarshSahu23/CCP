ed0be0aea393df74061860c638c48c3744f93070
Harsh Sahu <harshsahu7765858311@gmail.com>
2023-06-09 16:48:52 +0530

    Changes to be committed: 	new file:   p10.java

---
+++ p10.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+public class p10
+{
+    public static void main(String[] args)
+    {
+        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Printing elements in arrray : ");
+        for (int i : a) {
+            System.out.print(i+", ");
+        }
+        int evenSum=0,oddSum=0;
+        for (int j : a) {
+            if(j%2==0){
+                evenSum+=j;
+            }
+            else{
+                oddSum+=j;
+            }
+        }
+        System.out.println("\nSum of all even numbers = "+evenSum);
+        System.out.println("Sum of all odd numbers  = "+oddSum);
+
+    }
+}
